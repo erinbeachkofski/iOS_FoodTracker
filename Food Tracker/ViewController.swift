e3a1e19222dde37ee519b38155b2f2f3fd0bd9f8
Erin Beachkofski <beachko2@uwm.edu>
2020-01-06 12:34:17 -0600

    Initial Commit

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Erin Beachkofski on 1/6/20.
+//  Copyright © 2020 Erin Beachkofski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
