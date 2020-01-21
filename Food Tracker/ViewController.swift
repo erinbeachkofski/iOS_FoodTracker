ccb5390da543ddea6ebde2a05146f2fe22b03bff
Erin Beachkofski <beachko2@uwm.edu>
2020-01-21 16:07:22 -0600

    Added functionality to imagePicker and changed Pictures privacy

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var setDefaultLabelText: UIButton!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     
     override func viewDidLoad() {
@@ -33,4 +34,34 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = textField.text
     }
     
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+//        Hide keyboard
+        nameTextField.resignFirstResponder()
+//        Instantiate a view controller that allows user to choose images from their library
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+//        let's the image picker know when user picks an image
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        Dismiss picker if user canceled
+        dismiss(animated: true, completion: nil)
+
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        use the original image, not representations of image
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            else {
+            fatalError("Expected a dictionary containing an image, but was provided with the following: \(info)")
+        }
+//        Set photoImageView to display the selected image
+        photoImageView.image = selectedImage
+//        Dismiss imagePicker
+        dismiss(animated: true, completion: nil)
+    }
 }
